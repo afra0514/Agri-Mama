fee13f736a9b26d8374c6a5bfa252558d936a234
Syeda Afra Anam <syedafranam1405@gmail.com>
2026-09-06 03:37:02 +0600

    Added frontend App jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// Global styles (Make sure @import "tailwindcss"; is at the top of this file)
 import './App.css'; 
-
-// Context Provider
 import { AuthProvider } from './context/AuthContext';
-
-// Pages
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -18,14 +12,9 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          {/* Main Landing Page */}
           <Route path="/" element={<LandingPage />} />
-          
-          {/* Auth Pages */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          
-          {/* Main Chat Interface */}
           <Route path="/chat" element={<ChatWindow />} />
         </Routes>
       </BrowserRouter>
