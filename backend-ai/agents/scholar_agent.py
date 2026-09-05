f76584cf628c4b609978d10965b85b1fcbfa10ae
Syeda Afra Anam <syedafranam1405@gmail.com>
2026-09-06 04:13:30 +0600

    Added backend ai scholar agent

---
+++ backend-ai/agents/scholar_agent.py
@@ -0,0 +1,25 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools.search_tool import academic_search_tool
+from tools.search_tool import grounded_search_tool as internet_search_tool
+from tools.rag_tool import agri_knowledge_tool
+from tools.document_tool import analyze_document_text
+from constants.prompts.scholar_prompt import SCHOLAR_PROMPT
+
+def get_scholar_agent(llm):
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", SCHOLAR_PROMPT),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ])
+    
+    tools = [academic_search_tool, agri_knowledge_tool, internet_search_tool, analyze_document_text]
+    
+    agent = create_tool_calling_agent(llm, tools, prompt)
+    
+    return AgentExecutor(
+        agent=agent, 
+        tools=tools, 
+        verbose=True, 
+        handle_parsing_errors=True
+    )
