dfc75d7754198509915c482a0380a2f791431ed0
Syeda Afra Anam <syedafranam1405@gmail.com>
2026-09-06 03:59:12 +0600

    Added backend ai config

---
+++ backend-ai/config.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+os.environ["ANONYMIZED_TELEMETRY"] = "False"
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = "AgriMama"
+
+LLM_MODEL = "openai/gpt-oss-120b"
+VISION_MODEL = "qwen/qwen3.6-27b" 
+VOICE_MODEL = "whisper-large-v3" 
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CHROMA_DB_DIR = os.path.join(BASE_DIR, "chroma_db")
+KNOWLEDGE_DIR = os.path.join(BASE_DIR, "knowledge_base")
+UPLOAD_DIR = os.path.join(BASE_DIR, "temp_uploads")
+
+for folder in [CHROMA_DB_DIR, KNOWLEDGE_DIR, UPLOAD_DIR]:
+    os.makedirs(folder, exist_ok=True)
+
+print("AgriMama Configuration Loaded Successfully.")
