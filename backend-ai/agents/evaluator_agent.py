1ee6beaf0188afa2abe37e856c8ea6ff601d7a27
Syeda Afra Anam <syedafranam1405@gmail.com>
2026-09-06 20:28:05 +0600

    Modified evaluator agent

--- backend-ai/agents/evaluator_agent.py
+++ backend-ai/agents/evaluator_agent.py
@@ -2,10 +2,13 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from constants.prompts.evaluator_prompt import EVALUATOR_PROMPT
 
+# Create evaluator
 def get_evaluator_agent(llm):
+    # Define prompt
     prompt = ChatPromptTemplate.from_messages([
         ("system", EVALUATOR_PROMPT),
         ("human", "Original Response: {original_response}")
     ])
     
+    # Return chain
     return prompt | llm | StrOutputParser()
