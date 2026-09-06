2f61fa1b69d4b0a83b432d668a5b50fa782d6c84
Syeda Afra Anam <syedafranam1405@gmail.com>
2026-09-06 11:15:03 +0600

    Modified ocr tool

--- backend-ai/tools/ocr_tool.py
+++ backend-ai/tools/ocr_tool.py
@@ -8,8 +8,10 @@ def extract_text_from_image(file_path: str):
     Extracts Bengali and English text from images using EasyOCR. 
     Use this for reading seed packets, pesticide labels, or handwritten notes.
     """
+
     try:
         result = reader.readtext(file_path, detail=0)
         return " ".join(result)
+    
     except Exception as e:
         return f"OCR Error: {str(e)}"
