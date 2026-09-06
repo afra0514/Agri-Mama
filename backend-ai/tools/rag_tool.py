40fc03e91a312bdeee27323e942052ab534cc710
Syeda Afra Anam <syedafranam1405@gmail.com>
2026-09-06 11:15:39 +0600

    Modified rag tool

--- backend-ai/tools/rag_tool.py
+++ backend-ai/tools/rag_tool.py
@@ -4,4 +4,5 @@ from vector_db.retrieval import search_knowledge
 @tool
 def agri_knowledge_tool(query: str):
     """Consult this tool for farming advice, crop diseases, and agri-policies from documents."""
+    
     return search_knowledge(query, k=2)
