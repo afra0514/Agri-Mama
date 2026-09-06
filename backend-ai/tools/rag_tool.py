079a974bd025661d45cdc29f381315136ba09605
Syeda Afra Anam <syedafranam1405@gmail.com>
2026-09-06 21:00:08 +0600

    Updated rag tool

--- backend-ai/tools/rag_tool.py
+++ backend-ai/tools/rag_tool.py
@@ -1,8 +1,9 @@
-from langchain.tools import tool
-from vector_db.retrieval import search_knowledge
+from langchain.tools import tool 
+from vector_db.retrieval import search_knowledge 
 
 @tool
 def agri_knowledge_tool(query: str):
     """Consult this tool for farming advice, crop diseases, and agri-policies from documents."""
     
-    return search_knowledge(query, k=2)
+    # Search database
+    return search_knowledge(query, k=2) # Top 2 results
